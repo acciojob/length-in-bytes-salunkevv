f1f633e02f143d9340cb30b0f850a685b0bde550
salunkevv <86159929+salunkevv@users.noreply.github.com>
2023-11-18 11:10:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 const byteSize = (str) => {
   // write your code here
-	const blob = new Blob([JSON.stringify(str, null, 2)], {
-  type: "",
-		return blob.size;
-});
+	const blob = new Blob([str]).size;
+	return blob;
 };
 // Do not change the code below
+
 const str = prompt("Enter some string.");
 alert(byteSize(str));
